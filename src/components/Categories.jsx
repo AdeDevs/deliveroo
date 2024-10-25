43ebcc33137634c27ee770305be5f35db2090c6a
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-10-25 16:43:48 +0100

    updt

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,17 @@
 import { NavLink, Route, Routes } from "react-router-dom";
-// import Jollof from "../assets/jollof-bg-removebg-preview.png";
-// import CoconutRice from "../assets/food/coconut-rice.png";
-// import Amala from "../assets/food/amala.png";
-// import Coke from "../assets/food/coke.png";
-// import Chocolate from "../assets/food/Chocolate.png";
-// import Fanta from "../assets/food/Fanta.png";
-// import FriedRice from "../assets/food/fried-rice.png";
-// import Hollandia from "../assets/food/hollandia.png";
-// import VanillaIceCream from "../assets/food/ice-cream.png";
-// import Maltina from "../assets/food/maltina.png";
-// import PassionFruit from "../assets/food/PassionFruit.png";
-// import Pasta from "../assets/food/pasta.png";
-// import Vanilla from "../assets/food/Vanilla.png";
+import Jollof from "../assets/jollof-bg-removebg-preview.png";
+import CoconutRice from "../assets/food/coconut-rice.png";
+import Amala from "../assets/food/amala.png";
+import Coke from "../assets/food/coke.png";
+import Chocolate from "../assets/food/Chocolate.png";
+import Fanta from "../assets/food/Fanta.png";
+import FriedRice from "../assets/food/fried-rice.png";
+import Hollandia from "../assets/food/hollandia.png";
+import VanillaIceCream from "../assets/food/ice-cream.png";
+import Maltina from "../assets/food/maltina.png";
+import PassionFruit from "../assets/food/Passionfruit.png";
+import Pasta from "../assets/food/pasta.png";
+import Vanilla from "../assets/food/Vanilla.png";
 
 
 function CategoriesPage() {
@@ -27,11 +27,11 @@ function CategoriesPage() {
         </nav>
       </header>
       <Routes>
-      {/* <Route path="/" element={<Rice />}/>
+      <Route path="/" element={<Rice />}/>
       <Route path="swallow" element={<Swallow />} />
       <Route path="icecream" element={<IceCream />} />
       <Route path="chicken" element={<Chicken />} />
-      <Route path="drinks" element={<Drinks />} /> */}
+      <Route path="drinks" element={<Drinks />} />
       </Routes>
     </div>
   );
@@ -39,261 +39,261 @@ function CategoriesPage() {
 
 export default CategoriesPage;
 
-// function Rice() {
-//     return (
-//       <div className="menu">
-//       <div>
-//         <section className="food-image">
-//           <img src={Jollof} alt="Jollof" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Jollof Rice</h1>
-//             <p>₦ 3,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="food-image">
-//           <img src={FriedRice} alt="Fried Rice" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Fried Rice</h1>
-//             <p>₦ 3,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="food-image">
-//           <img src={CoconutRice} alt="Coconut Rice" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Coconut Rice</h1>
-//             <p>₦ 4,000</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="food-image">
-//           <img src={Pasta} alt="Pasta" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Pasta</h1>
-//             <p>₦ 4,000</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//     </div>
-//     )
-// }
+function Rice() {
+    return (
+      <div className="menu">
+      <div>
+        <section className="food-image">
+          <img src={Jollof} alt="Jollof" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Jollof Rice</h1>
+            <p>₦ 3,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="food-image">
+          <img src={FriedRice} alt="Fried Rice" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Fried Rice</h1>
+            <p>₦ 3,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="food-image">
+          <img src={CoconutRice} alt="Coconut Rice" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Coconut Rice</h1>
+            <p>₦ 4,000</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="food-image">
+          <img src={Pasta} alt="Pasta" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Pasta</h1>
+            <p>₦ 4,000</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+    </div>
+    )
+}
 
-// function Swallow() {
-//     return (
-//       <div className="menu">
-//       <div>
-//         <section className="food-image">
-//           <img src={Amala} alt="Amala" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Amala</h1>
-//             <p>₦ 2,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//     </div>
-//     )
-// }
+function Swallow() {
+    return (
+      <div className="menu">
+      <div>
+        <section className="food-image">
+          <img src={Amala} alt="Amala" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Amala</h1>
+            <p>₦ 2,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+    </div>
+    )
+}
 
-// function IceCream() {
-//     return (
-//       <div className="menu">
-//       <div>
-//         <section className="food-image">
-//           <img src={Vanilla} alt="VanillaIceCream" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Vanilla Ice Cream</h1>
-//             <p>₦ 5,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="food-image">
-//           <img src={Chocolate} alt="Chocolate" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Chocolate Ice Cream</h1>
-//             <p>₦ 5,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="food-image">
-//           <img src={PassionFruit} alt="PassionFruit" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Passionfruit Ice Cream</h1>
-//             <p>₦ 5,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="food-image">
-//           <img src={VanillaIceCream} alt="VanillaIceCream" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Chocolate Fudge Brownie</h1>
-//             <p>₦ 4,500</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//     </div>
-//     )
-// }
+function IceCream() {
+    return (
+      <div className="menu">
+      <div>
+        <section className="food-image">
+          <img src={Vanilla} alt="VanillaIceCream" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Vanilla Ice Cream</h1>
+            <p>₦ 5,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="food-image">
+          <img src={Chocolate} alt="Chocolate" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Chocolate Ice Cream</h1>
+            <p>₦ 5,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="food-image">
+          <img src={PassionFruit} alt="PassionFruit" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Passionfruit Ice Cream</h1>
+            <p>₦ 5,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="food-image">
+          <img src={VanillaIceCream} alt="VanillaIceCream" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Chocolate Fudge Brownie</h1>
+            <p>₦ 4,500</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+    </div>
+    )
+}
 
-// function Chicken() {
-//     return (
-//       <div className="menu">
-//       <div>
-//         <h1>There's nothing here</h1>
-//       </div>
-//     </div>
-//     )
-// }
+function Chicken() {
+    return (
+      <div className="menu">
+      <div>
+        <h1>There's nothing here</h1>
+      </div>
+    </div>
+    )
+}
 
-// function Drinks() {
-//     return (
-//       <div className="menu">
-//       <div>
-//         <section className="drink-image">
-//           <img src={Coke} alt="Coke" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Coke</h1>
-//             <p>₦ 700</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="drink-image">
-//           <img src={Fanta} alt="Fanta" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Fanta</h1>
-//             <p>₦ 700</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="drink-image">
-//           <img src={Hollandia} alt="Hollandia" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Hollandia</h1>
-//             <p>₦ 2,000</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//       <div>
-//         <section className="drink-image">
-//           <img src={Maltina} alt="Maltina" />
-//         </section>
-//         <section className="food-info">
-//           <aside>
-//             <h1>Maltina</h1>
-//             <p>₦ 800</p>
-//           </aside>
-//           <aside>
-//             <h2>
-//               {" "}
-//               <ion-icon name="cart-outline"></ion-icon>{" "}
-//             </h2>
-//           </aside>
-//         </section>
-//       </div>
-//     </div>
-//     )
-// }
+function Drinks() {
+    return (
+      <div className="menu">
+      <div>
+        <section className="drink-image">
+          <img src={Coke} alt="Coke" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Coke</h1>
+            <p>₦ 700</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="drink-image">
+          <img src={Fanta} alt="Fanta" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Fanta</h1>
+            <p>₦ 700</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="drink-image">
+          <img src={Hollandia} alt="Hollandia" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Hollandia</h1>
+            <p>₦ 2,000</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+      <div>
+        <section className="drink-image">
+          <img src={Maltina} alt="Maltina" />
+        </section>
+        <section className="food-info">
+          <aside>
+            <h1>Maltina</h1>
+            <p>₦ 800</p>
+          </aside>
+          <aside>
+            <h2>
+              {" "}
+              <ion-icon name="cart-outline"></ion-icon>{" "}
+            </h2>
+          </aside>
+        </section>
+      </div>
+    </div>
+    )
+}
